b198deecf94d581d726a5ab6d4bd5a7231b050f6
Ken Okabe <ken@design-fun.com>
2018-01-21 01:00:11 +0900

    implement  bme driver

--- BME280_PIC.X/main.c
+++ BME280_PIC.X/main.c
@@ -41,8 +41,10 @@
 */
 #include <stdint.h>
 #include "mcc_generated_files/mcc.h"
+#include "BME280_driver/bme280.h"
 
 uint8_t BME280_Read(uint8_t reg, uint8_t *pData);
+int8_t user_i2c_read(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len);
 
 #define BME280_ADDRESS  0x76   // slave device address
 #define BME280_ADDR_ID  0xD0
@@ -52,7 +54,8 @@ uint8_t BME280_Read(uint8_t reg, uint8_t *pData);
  */
 void main(void)
 {
-    uint8_t data;
+    static uint8_t data[27];
+    int i;
     
     // initialize the device
     SYSTEM_Initialize();
@@ -77,13 +80,19 @@ void main(void)
 //    D5_LAT=0b1;
     D6_LAT=0b1;
 //    D7_LAT=0b1;
-
+    __delay_ms(3000);
     printf("BME280 PIC start!\r\n");
     
-    BME280_Read(0xD0,&data);
-    printf("BME280 ID:0x%02x\r\n",data);
-
-    
+//    BME280_Read(0xD0,&data);
+    user_i2c_read(BME280_ADDRESS,BME280_ADDR_ID,data,1);
+    printf("BME280 ID:0x%02x\r\n",data[0]);
+    D7_LAT=0b1;    
+    user_i2c_read(BME280_ADDRESS,0x88,data,26);
+    printf("Calib T: ");
+    for(i=0;i<26;i++){
+        printf("0x%02x ",data[i]);
+    }
+    printf("\r\n");
     
     while (1)
     {
@@ -108,6 +117,86 @@ uint8_t BME280_Read(uint8_t reg, uint8_t *pData)
     return (status == I2C1_MESSAGE_COMPLETE); 
 }
 
+void user_delay_ms(uint32_t period)
+{
+    /*
+     * Return control or wait,
+     * for a period amount of milliseconds
+     */
+    __delay_ms(period);
+    
+    return;
+}
+ 
+
+int8_t user_i2c_read(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len)
+{
+    int8_t rslt = 0; /* Return 0 for Success, non-zero for failure */
+    I2C1_MESSAGE_STATUS status = I2C1_MESSAGE_PENDING;
+    static I2C1_TRANSACTION_REQUEST_BLOCK trb[2];
+    /*
+     * The parameter dev_id can be used as a variable to store the I2C address of the device
+     */
+ 
+    /*
+     * Data on the bus should be like
+     * |------------+---------------------|
+     * | I2C action | Data                |
+     * |------------+---------------------|
+     * | Start      | -                   |
+     * | Write      | (reg_addr)          |
+     * | Stop       | -                   |
+     * | Start      | -                   |
+     * | Read       | (reg_data[0])       |
+     * | Read       | (....)              |
+     * | Read       | (reg_data[len - 1]) |
+     * | Stop       | -                   |
+     * |------------+---------------------|
+     */
+   
+    
+    if(len>26){
+        printf("I2C len over\r\n");
+        return -1;
+    }
+ 
+    I2C1_MasterWriteTRBBuild(&trb[0], &reg_addr, 1, dev_id);
+//    for(i=0;i<len;i++){
+        I2C1_MasterReadTRBBuild(&trb[1], reg_data, len, dev_id);
+//    }
+    I2C1_MasterTRBInsert(2, &trb[0], &status);
+ 
+    while(status == I2C1_MESSAGE_PENDING);      // blocking
+    
+    rslt = status;
+
+    return rslt;
+}
+ 
+int8_t user_i2c_write(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len)
+{
+    int8_t rslt = 0; /* Return 0 for Success, non-zero for failure */
+ 
+    /*
+     * The parameter dev_id can be used as a variable to store the I2C address of the device
+     */
+ 
+    /*
+     * Data on the bus should be like
+     * |------------+---------------------|
+     * | I2C action | Data                |
+     * |------------+---------------------|
+     * | Start      | -                   |
+     * | Write      | (reg_addr)          |
+     * | Write      | (reg_data[0])       |
+     * | Write      | (....)              |
+     * | Write      | (reg_data[len - 1]) |
+     * | Stop       | -                   |
+     * |------------+---------------------|
+     */
+ 
+    return rslt;
+}
 /**
  End of File
 */
--- BME280_PIC.X/nbproject/Makefile-default.mk
+++ BME280_PIC.X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=mcc_generated_files/pin_manager.c mcc_generated_files/mcc.c mcc_generated_files/eusart1.c mcc_generated_files/interrupt_manager.c mcc_generated_files/i2c1.c main.c mcc_generated_files/adc.c
+SOURCEFILES_QUOTED_IF_SPACED=mcc_generated_files/pin_manager.c mcc_generated_files/mcc.c mcc_generated_files/eusart1.c mcc_generated_files/interrupt_manager.c mcc_generated_files/i2c1.c main.c mcc_generated_files/adc.c BME280_driver/bme280.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/mcc_generated_files/pin_manager.p1 ${OBJECTDIR}/mcc_generated_files/mcc.p1 ${OBJECTDIR}/mcc_generated_files/eusart1.p1 ${OBJECTDIR}/mcc_generated_files/interrupt_manager.p1 ${OBJECTDIR}/mcc_generated_files/i2c1.p1 ${OBJECTDIR}/main.p1 ${OBJECTDIR}/mcc_generated_files/adc.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/mcc_generated_files/pin_manager.p1.d ${OBJECTDIR}/mcc_generated_files/mcc.p1.d ${OBJECTDIR}/mcc_generated_files/eusart1.p1.d ${OBJECTDIR}/mcc_generated_files/interrupt_manager.p1.d ${OBJECTDIR}/mcc_generated_files/i2c1.p1.d ${OBJECTDIR}/main.p1.d ${OBJECTDIR}/mcc_generated_files/adc.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/mcc_generated_files/pin_manager.p1 ${OBJECTDIR}/mcc_generated_files/mcc.p1 ${OBJECTDIR}/mcc_generated_files/eusart1.p1 ${OBJECTDIR}/mcc_generated_files/interrupt_manager.p1 ${OBJECTDIR}/mcc_generated_files/i2c1.p1 ${OBJECTDIR}/main.p1 ${OBJECTDIR}/mcc_generated_files/adc.p1 ${OBJECTDIR}/BME280_driver/bme280.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/mcc_generated_files/pin_manager.p1.d ${OBJECTDIR}/mcc_generated_files/mcc.p1.d ${OBJECTDIR}/mcc_generated_files/eusart1.p1.d ${OBJECTDIR}/mcc_generated_files/interrupt_manager.p1.d ${OBJECTDIR}/mcc_generated_files/i2c1.p1.d ${OBJECTDIR}/main.p1.d ${OBJECTDIR}/mcc_generated_files/adc.p1.d ${OBJECTDIR}/BME280_driver/bme280.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/mcc_generated_files/pin_manager.p1 ${OBJECTDIR}/mcc_generated_files/mcc.p1 ${OBJECTDIR}/mcc_generated_files/eusart1.p1 ${OBJECTDIR}/mcc_generated_files/interrupt_manager.p1 ${OBJECTDIR}/mcc_generated_files/i2c1.p1 ${OBJECTDIR}/main.p1 ${OBJECTDIR}/mcc_generated_files/adc.p1
+OBJECTFILES=${OBJECTDIR}/mcc_generated_files/pin_manager.p1 ${OBJECTDIR}/mcc_generated_files/mcc.p1 ${OBJECTDIR}/mcc_generated_files/eusart1.p1 ${OBJECTDIR}/mcc_generated_files/interrupt_manager.p1 ${OBJECTDIR}/mcc_generated_files/i2c1.p1 ${OBJECTDIR}/main.p1 ${OBJECTDIR}/mcc_generated_files/adc.p1 ${OBJECTDIR}/BME280_driver/bme280.p1
 
 # Source Files
-SOURCEFILES=mcc_generated_files/pin_manager.c mcc_generated_files/mcc.c mcc_generated_files/eusart1.c mcc_generated_files/interrupt_manager.c mcc_generated_files/i2c1.c main.c mcc_generated_files/adc.c
+SOURCEFILES=mcc_generated_files/pin_manager.c mcc_generated_files/mcc.c mcc_generated_files/eusart1.c mcc_generated_files/interrupt_manager.c mcc_generated_files/i2c1.c main.c mcc_generated_files/adc.c BME280_driver/bme280.c
 
 
 CFLAGS=
@@ -149,6 +149,14 @@ ${OBJECTDIR}/mcc_generated_files/adc.p1: mcc_generated_files/adc.c  nbproject/Ma
 	@-${MV} ${OBJECTDIR}/mcc_generated_files/adc.d ${OBJECTDIR}/mcc_generated_files/adc.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/mcc_generated_files/adc.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
+${OBJECTDIR}/BME280_driver/bme280.p1: BME280_driver/bme280.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}/BME280_driver" 
+	@${RM} ${OBJECTDIR}/BME280_driver/bme280.p1.d 
+	@${RM} ${OBJECTDIR}/BME280_driver/bme280.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --opt=+asm,-asmfile,-speed,+space,-debug,-local --addrqual=ignore --mode=pro -P -N255 --warn=0 --asmlist -DXPRJ_default=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/BME280_driver/bme280.p1  BME280_driver/bme280.c 
+	@-${MV} ${OBJECTDIR}/BME280_driver/bme280.d ${OBJECTDIR}/BME280_driver/bme280.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/BME280_driver/bme280.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 else
 ${OBJECTDIR}/mcc_generated_files/pin_manager.p1: mcc_generated_files/pin_manager.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/mcc_generated_files" 
@@ -205,6 +213,14 @@ ${OBJECTDIR}/mcc_generated_files/adc.p1: mcc_generated_files/adc.c  nbproject/Ma
 	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --opt=+asm,-asmfile,-speed,+space,-debug,-local --addrqual=ignore --mode=pro -P -N255 --warn=0 --asmlist -DXPRJ_default=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/mcc_generated_files/adc.p1  mcc_generated_files/adc.c 
 	@-${MV} ${OBJECTDIR}/mcc_generated_files/adc.d ${OBJECTDIR}/mcc_generated_files/adc.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/mcc_generated_files/adc.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/BME280_driver/bme280.p1: BME280_driver/bme280.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}/BME280_driver" 
+	@${RM} ${OBJECTDIR}/BME280_driver/bme280.p1.d 
+	@${RM} ${OBJECTDIR}/BME280_driver/bme280.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --opt=+asm,-asmfile,-speed,+space,-debug,-local --addrqual=ignore --mode=pro -P -N255 --warn=0 --asmlist -DXPRJ_default=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/BME280_driver/bme280.p1  BME280_driver/bme280.c 
+	@-${MV} ${OBJECTDIR}/BME280_driver/bme280.d ${OBJECTDIR}/BME280_driver/bme280.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/BME280_driver/bme280.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
